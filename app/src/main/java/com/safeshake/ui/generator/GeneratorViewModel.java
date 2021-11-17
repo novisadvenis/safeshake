b39e012f547a3145a5eac5b4a8c35a491393ecf1
Kaushall Vimalarajah <v.kaushi4@gmail.com>
2021-11-17 14:58:30 +0100

    Generator

--- app/src/main/java/com/safeshake/ui/generator/GeneratorViewModel.java
+++ app/src/main/java/com/safeshake/ui/generator/GeneratorViewModel.java
@@ -10,7 +10,7 @@ public class GeneratorViewModel extends ViewModel {
 
     public GeneratorViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        //mText.setValue("This is dashboard fragment");
     }
 
     public LiveData<String> getText() {
